0ce4f5ef04a2cc688f251f815df14b57dc869182
breannams <breannamarie_s@hotmail.com>
2021-01-31 00:26:45 -0500

    commit tree 16903cac9b0a5a14b1a101ed743cf5afce40220c

---
+++ lib/song.rb
@@ -0,0 +1,63 @@
+class Song
+    
+    attr_accessor :name
+    attr_reader :artist, :genre
+    @@all = []
+
+    def initialize(name, artist = nil, genre = nil)
+        @name = name
+       self.artist = artist if artist
+       self.genre = genre if genre
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def self.create(name)
+         song = new(name)
+         song.save
+         song
+    end
+
+    def artist=(artist)
+        @artist = artist
+        artist.add_song(self)
+    end
+    def genre=(genre)
+        @genre = genre
+       genre.songs << self if !genre.songs.include?(self)
+    end
+
+    def self.find_by_name(name)
+        self.all.find {|o| o.name == name}
+       end
+     
+    def self.find_or_create_by_name(name)
+           if self.find_by_name(name) == nil
+              self.create(name)
+           else
+           self.find_by_name(name)
+           end
+    end
+
+    def self.new_from_filename(file)
+       file = file.gsub(".mp3", "")
+       artist,song,genre = file.split(" - ")
+       song_artist = Artist.find_or_create_by_name(artist)
+       song_genre = Genre.find_or_create_by_name(genre)
+       new_song = Song.new(song, song_artist, song_genre)
+    end
+
+    def self.create_from_filename(file)
+       self.new_from_filename(file).tap {|s| s.save}
+     end
+end
